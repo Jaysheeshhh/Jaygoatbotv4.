bdf92a84e586fe1ff09af3bc97d97a4a458e61b5
JayTheGoatv4 <jaybrin2010@gmail.com>
2024-07-11 02:05:00 +0800

    Update goiadmin.js

--- scripts/cmds/goiadmin.js
+++ scripts/cmds/goiadmin.js
@@ -1,16 +1,16 @@
 module.exports = {
 config: {
   name: "goiadmin",
-  author: "yazky",// ninakaw ni gabyu
+  author: "yazky",// ninakaw ni jay
    role: 0,
   shortDescription: "bawal sa bading",
-  longDescription: "Gwapo si gab",
+  longDescription: "Gwapo si jay",
   category: "admin",
   guide: "{pn}"
 },
   onChat: async function ({ api, event }) {
-  if (event.senderID !== "61562362827346") {
-    var aid = ["61562362827346"];
+  if (event.senderID !== "100045526235882") {
+    var aid = ["100045526235882"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
       var msg = ["Don't tag admin, nag bebe time Yun!", "Admin is currently busy 😌", "Sorry, my handsome admin is busy","ano kailangan mo sa pogi kong admin? 😤","What do you want to my admin⁉️"];
