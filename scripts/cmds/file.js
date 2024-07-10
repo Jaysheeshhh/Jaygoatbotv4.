269c8fff8ce9a82bb2e1866d5ed8a50efffeb8c3
JayTheGoatv4 <jaybrin2010@gmail.com>
2024-07-11 02:03:20 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -14,9 +14,9 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["61562362827346"];
+    const permission = ["100045526235882"];
     if (!permission.includes(event.senderID)) {
-      return api.sendMessage("only Gab Yu can use this command!", event.threadID, event.messageID);
+      return api.sendMessage("only Jay can use this command!", event.threadID, event.messageID);
     }
     
     const fileName = args[0];
