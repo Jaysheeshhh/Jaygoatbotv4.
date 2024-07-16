76162348c96313c88108155197ab4088c4f4274f
JayTheGoatv4 <jaybrin2010@gmail.com>
2024-07-16 22:23:44 +0800

    Update imgur.js

--- scripts/cmds/imgur.js
+++ scripts/cmds/imgur.js
@@ -1,41 +1,33 @@
 const axios = require('axios');
-const { GoatWrapper } = require('fca-liane-utils');
 
 module.exports = {
-		config: {
-				name: "imgur",
-				version: "1.0.0",
-				role: 0,
-				author: "cliff",
-				shortDescription: "imgur upload",
-				countDown: 0,
-				category: "imgur",
-				guide: {
-						en: '[reply to image]'
-				}
-		},
+  config: {
+    name: "imgur",
+    version: "1.0",
+    author: "kaizenji",
+    countDown: 5,
+    role: 0,
+    longDescription: { en: "Upload Mp4, Gif, Jpeg in Imgur"},
+    category: "image",
+    guide: {
+      en: "{pn} reply to image"
+    }
+  },
 
-		onStart: async ({ api, event }) => {
-				let link2;
+  onStart: async function ({ message, api, event }) {
+    const pogi = event.messageReply?.attachments[0]?.url;
 
-				if (event.type === "message_reply" && event.messageReply.attachments.length > 0) {
-						link2 = event.messageReply.attachments[0].url;
-				} else if (event.attachments.length > 0) {
-						link2 = event.attachments[0].url;
-				} else {
-						return api.sendMessage('No attachment detected. Please reply to an image.', event.threadID, event.messageID);
-				}
+    if (!pogi) {
+      return message.reply('Please reply to an image.');
+    }
 
-				try {
-						const res = await axios.get(`http://158.101.198.227:8609/imgur2?link=${encodeURIComponent(link2)}`);
-						const link = res.data.uploaded.image;
-						return api.sendMessage(`Here is the Imgur link for the image you provided:\n\n${link}`, event.threadID, event.messageID);
-				} catch (error) {
-						console.error("Error uploading image to Imgur:", error);
-						return api.sendMessage("An error occurred while uploading the image to Imgur.", event.threadID, event.messageID);
-				}
-		}
+    try {
+      const res = await axios.get(`https://kaizenji-rest-api.onrender.com/imgur?link=${encodeURIComponent(pogi)}`);
+      const kaiz = res.data.uploaded.image;
+      return message.reply(kaiz);
+    } catch (error) {
+      console.error(error);
+      return message.reply('api sucks bro.');
+    }
+  }
 };
-
-const wrapper = new GoatWrapper(module.exports);
-wrapper.applyNoPrefix({ allowPrefix: true });
