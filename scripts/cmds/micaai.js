e3db2b67ad7fac8d632482eb35e428e02e5a4b23
JayTheGoatv4 <jaybrin2010@gmail.com>
2024-07-16 20:34:12 +0800

    Create micaai.js

---
+++ scripts/cmds/micaai.js
@@ -0,0 +1,60 @@
+const axios = require('axios');
+
+async function a(api, event, args, message) {
+  try {
+    const a = args.join(" ").trim();
+
+    if (!a) {
+      return message.reply("ex: {p}mica {prompt} ");
+    }
+
+    const b = "you are mica ai"; // the more better content you give the  best it became
+    const c = await d(a, b);
+
+    if (c.code === 2 && c.message === "success") {
+      message.reply(c.answer, (r, s) => {
+        global.GoatBot.onReply.set(s.messageID, {
+          commandName: module.exports.config.name,
+          uid: event.senderID 
+        });
+      });
+    } else {
+      message.reply("Please try again later.");
+    }
+  } catch (e) {
+    console.error("Error:", e);
+    message.reply("An error occurred while processing your request.");
+  }
+}
+
+async function d(a, b) {
+  try {
+    const d = await axios.get(`https://personal-ai-phi.vercel.app/kshitiz?prompt=${encodeURIComponent(a)}&content=${encodeURIComponent(b)}`);
+    return d.data;
+  } catch (f) {
+    console.error("Error from api", f.message);
+    throw f;
+  }
+}
+
+module.exports = {
+  config: {
+    name: "mica",
+    version: "1.0",
+    author: "Gab Yu", // laplapin ang kiffy woooh!!
+    role: 0,
+    longDescription: "mica ai",
+    category: "ai",
+    guide: {
+      en: "{p}mica [prompt]"
+    }
+  },
+  
+  handleCommand: a,
+  onStart: function ({ api, message, event, args }) {
+    return a(api, event, args, message);
+  },
+  onReply: function ({ api, message, event, args }) {
+    return a(api, event, args, message);
+  }
+};
