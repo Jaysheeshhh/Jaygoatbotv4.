d8adb55f099edbaf6d6b9baf9844be0e59c97688
JayTheGoatv4 <jaybrin2010@gmail.com>
2024-07-21 12:04:01 +0800

    Update autotime.js

--- scripts/cmds/autotime.js
+++ scripts/cmds/autotime.js
@@ -12,78 +12,78 @@ module.exports.config = {
 
 module.exports.onLoad = async ({ api }) => {
   const arrayData = {
-     "12:00:00 PM": {
-        message: "┃📌 good afternoon everyone don't forget to eat y'all lunch break🍛"
-      },
-      "01:00:00 AM": {
-        message: "┃📌 good morning everyone!!, have a nice morning🍞☕🌅"
-      },
-      "02:00:00 AM": {
-        message: "┃📌 don't forget to add/follow my owner☺.\n\n📩: https://www.facebook.com/kyledev03"
-      },
-      "03:00:00 AM": {
-        message: "┃📌 aga nyo nagising ahh"
-      },
-      "04:00:00 AM": {
-        message: "┃📌 eyyy🤙don't panic it's organic eyyyyy🤙"
-      },
-      "05:00:00 AM": {
-        message: "┃📌 aga nyo nagising ahh sanaol strong💀🙏"
-      },
-      "06:00:00 AM": {
-        message: "┃📌 kape muna kayo☕"
-      },
-      "07:00:00 AM": {
-        message: "┃📌 don't forget to eat y'all breakfast!! 🍞☕🍛"
-      },
-      "08:00:00 AM": {
-        message: "┃📌 life update: pogi parin owner ko"
-      },
-      "09:00:00 AM": {
-        message: "┃📌 baka hinde pa kayo kumain kain na kayo💀🙏"
-      },
-      "10:00:00 AM": {
-        message: "┃📌 wag mo kalimutan e chat owner ko💀🙏"
-      },
-      "11:00:00 AM": {
-        message: "┃📌 hinde mababawasan kapogian ng owner ko, btw have a nice morning everyone!!"
-      },
-      "12:00:00 PM": {
-        message: "┃📌 kain na kayo mga lods💀"
-      },
-      "01:00:00 PM": {
-        message: "┃📌 don't forget to eat y'all lunch break😸"
-      },
-      "02:00:00 PM": {
-        message: "┃📌 good afternoon!!, my owner is so handsome asf😎"
-      },
-      "03:00:00 PM": {
-        message: "┃📌 miss ko na sya:("
-      },
-      "04:00:00 PM": {
-        message: "┃📌 magandang hapon mga lods😸"
-      },
-      "05:00:00 PM": {
-        message: "┃📌 pogi ng owner ko na si Jay 😎"
-      },
-      "06:00:00 PM": {
-        message: "┃📌 don't forget to eat y'all dinner💀🙏"
-      },
-      "07:00:00 PM": {
-        message: "┃📌 ano silbe ng pag online mo kung hinde mo din naman e chachat owner ko😎"
-      },
-      "08:00:00 PM": {
-        message: "┃📌 kumain naba kayo?"
-      },
-      "09:00:00 PM": {
-        message: "┃📌 matulog na kayo mga hangal😸"
-      },
-      "10:00:00 PM": {
-        message: "┃📌 gabi na nag puyat parin kayo💀🙏"
-      },
-      "11:00:00 PM": {
-        message: "┃📌 hinde mababawasan kapogian ng owner ko."
-      }
+    "12:00:00 PM": {
+      message: "┃📌 good afternoon everyone don't forget to eat y'all lunch break🍛"
+    },
+    "01:00:00 AM": {
+      message: "┃📌 good morning everyone!!, have a nice morning🍞☕🌅"
+    },
+    "02:00:00 AM": {
+      message: "┃📌 don't forget to add/follow my owner☺.\n\n📩: https://www.facebook.com/jayboy.pillonar?mibextid=ZbWKwL"
+    },
+    "03:00:00 AM": {
+      message: "┃📌 aga nyo nagising ahh"
+    },
+    "04:00:00 AM": {
+      message: "┃📌 eyyy🤙don't panic it's organic eyyyyy🤙"
+    },
+    "05:00:00 AM": {
+      message: "┃📌 aga nyo nagising ahh sanaol strong💀🙏"
+    },
+    "06:00:00 AM": {
+      message: "┃📌 kape muna kayo☕"
+    },
+    "07:00:00 AM": {
+      message: "┃📌 don't forget to eat y'all breakfast!! 🍞☕🍛"
+    },
+    "08:00:00 AM": {
+      message: "┃📌 life update: pogi parin owner ko"
+    },
+    "09:00:00 AM": {
+      message: "┃📌 baka hinde pa kayo kumain kain na kayo💀🙏"
+    },
+    "10:00:00 AM": {
+      message: "┃📌 wag mo kalimutan e chat owner ko💀🙏"
+    },
+    "11:00:00 AM": {
+      message: "┃📌 hinde mababawasan kapogian ng owner ko, btw have a nice morning everyone!!"
+    },
+    "12:00:00 PM": {
+      message: "┃📌 kain na kayo mga lods💀"
+    },
+    "01:00:00 PM": {
+      message: "┃📌 don't forget to eat y'all lunch break😸"
+    },
+    "02:00:00 PM": {
+      message: "┃📌 good afternoon!!, my owner is so handsome asf😎"
+    },
+    "03:00:00 PM": {
+      message: "┃ 📌 pogi ng owner ko na si Jay 😎"
+    },
+    "04:00:00 PM": {
+      message: "┃📌 magandang hapon mga lods😸"
+    },
+    "05:00:00 PM": {
+      message: "┃📌 @everyone pictorial na🔥🔥"
+    },
+    "06:00:00 PM": {
+      message: "┃📌 don't forget to eat y'all dinner💀🙏"
+    },
+    "07:00:00 PM": {
+      message: "┃📌 ano silbe ng pag online mo kung hinde mo din naman e chachat owner ko😎"
+    },
+    "08:00:00 PM": {
+      message: "┃📌 kumain naba kayo?"
+    },
+    "09:00:00 PM": {
+      message: "┃📌 matulog na kayo mga hangal😸"
+    },
+    "10:00:00 PM": {
+      message: "┃📌 gabi na nag puyat parin kayo💀🙏"
+    },
+    "11:00:00 PM": {
+      message: "┃📌 hinde mababawasan kapogian ng owner ko."
+    }
   };
 
   const checkTimeAndSendMessage = () => {
